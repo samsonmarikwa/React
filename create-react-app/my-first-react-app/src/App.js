1470433ecedb27887fcf88d0fcd2c5141d9a244f
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-01 22:11:09 -0400

    Pass a callback function as a recommended way to update state

--- create-react-app/my-first-react-app/src/App.js
+++ create-react-app/my-first-react-app/src/App.js
@@ -2,27 +2,32 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [name, setName] = useState('Annet');
   const [events, setEvents] = useState([
     { title: "Mario's birthday bash", id: 1 },
     { title: "Bowser's live stream", id: 2 },
     { title: 'Race on moo moo farm', id: 3 },
   ]);
 
-  const handleClick = () => {
-    setName('Samson');
-    console.log(name);
+  const handleClick = (id) => {
+    console.log(id);
+    // setEvents(events.filter((event) => event.id !== id));
+
+    // This is the right way of doing it because directly using the state value
+    // can cause problems if it was mutated by another process as the setState is
+    // asynchronous.
+    setEvents((prevEvents) => prevEvents.filter((event) => event.id !== id));
   };
 
   return (
     <div className='App'>
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change name</button>
-      {events.map((event, index) => (
-        <h2 key={event.id}>
-          {index} - {event.title}
-        </h2>
-      ))}
+      {events.map((event) => {
+        return (
+          <dir key={event.id}>
+            <h2>{event.title}</h2>
+            <button onClick={() => handleClick(event.id)}>delete event</button>
+          </dir>
+        );
+      })}
     </div>
   );
 }
