cc94a4a1619f713a545d3d26e0ccc17a539ea56d
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-04 20:54:19 -0400

    Component inline styles using the style object

--- create-react-app/my-first-react-app/src/component/Modal.js
+++ create-react-app/my-first-react-app/src/component/Modal.js
@@ -6,7 +6,13 @@ export default function Modal({ children, handleClose }) {
   return createPortal(
     <div className='modal-backdrop'>
       {/* <div className='modal'>{props.children}</div> */}
-      <div className='modal'>
+      <div
+        className='modal'
+        style={{
+          border: '4px solid',
+          borderColor: '#ff4500',
+          textAlign: 'center',
+        }}>
         {children}
         <button onClick={handleClose}>close</button>
       </div>
