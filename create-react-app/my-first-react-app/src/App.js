a23d9e5c5de4285fabefbfa561993c08c69691b3
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-04 20:59:46 -0400

    Component dynamic styles based on a prop value

--- create-react-app/my-first-react-app/src/App.js
+++ create-react-app/my-first-react-app/src/App.js
@@ -50,7 +50,7 @@ function App() {
         <p>Use the code NINJA10 at the checkout.</p>
       </Modal> */}
       {showModal && (
-        <Modal handleClose={handleClose}>
+        <Modal handleClose={handleClose} isSalesModal={false}>
           <h2>Terms and Conditions</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
--- create-react-app/my-first-react-app/src/component/Modal.js
+++ create-react-app/my-first-react-app/src/component/Modal.js
@@ -2,7 +2,7 @@ import { createPortal } from 'react-dom';
 import './Modal.css';
 
 // export default function Modal(props) {
-export default function Modal({ children, handleClose }) {
+export default function Modal({ children, handleClose, isSalesModal }) {
   return createPortal(
     <div className='modal-backdrop'>
       {/* <div className='modal'>{props.children}</div> */}
@@ -10,7 +10,7 @@ export default function Modal({ children, handleClose }) {
         className='modal'
         style={{
           border: '4px solid',
-          borderColor: '#ff4500',
+          borderColor: isSalesModal ? '#ff4500' : '#555',
           textAlign: 'center',
         }}>
         {children}
