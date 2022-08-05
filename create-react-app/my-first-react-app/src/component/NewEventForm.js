54438d1dbe3cde27bc5b63b2569a4a371b24198a
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-05 15:21:39 -0400

    use the useRef instead of useState to manage user input by reference a DOM element

--- create-react-app/my-first-react-app/src/component/NewEventForm.js
+++ create-react-app/my-first-react-app/src/component/NewEventForm.js
@@ -1,23 +1,34 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import { useRef } from 'react';
 import './NewEventForm.css';
 
 export default function NewEventForm({ addEvent }) {
-  const [title, setTitle] = useState('');
-  const [date, setDate] = useState('');
+  // const [title, setTitle] = useState('');
+  // const [date, setDate] = useState('');
+  const title = useRef(); // allows us to associate with a DOM element
+  const date = useRef();
 
   const resetForm = () => {
-    setTitle('');
-    setDate('');
+    // setTitle('');
+    // setDate('');
+    title.current.value = '';
+    date.current.value = '';
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    console.log(title, date);
     const event = {
-      title: title,
-      date: date,
+      title: title.current.value,
+      date: date.current.value,
       id: Math.floor(Math.random() * 10000),
     };
+
+    // const event = {
+    //   title: title,
+    //   date: date,
+    //   id: Math.floor(Math.random() * 10000),
+    // };
     addEvent(event);
     resetForm();
   };
@@ -28,16 +39,18 @@ export default function NewEventForm({ addEvent }) {
         <span>Event Title:</span>
         <input
           type='text'
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
+          // onChange={(e) => setTitle(e.target.value)}
+          // value={title}
+          ref={title}
         />
       </label>
       <label>
         <span>Event Date:</span>
         <input
           type='date'
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
+          // onChange={(e) => setDate(e.target.value)}
+          // value={date}
+          ref={date}
         />
       </label>
       <button>Submit</button>
