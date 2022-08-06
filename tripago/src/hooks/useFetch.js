9c524c067612f7433636ac904622d8ec42188cc1
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-06 01:42:06 -0400

    Aborting fetch Requests in async or stream operations where the results may try to update unmounted DOM elements - implement AbortController

--- tripago/src/hooks/useFetch.js
+++ tripago/src/hooks/useFetch.js
@@ -6,11 +6,13 @@ export const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const controller = new AbortController();
+
     const fetchData = async () => {
       setIsPending(true);
 
       try {
-        const res = await fetch(url);
+        const res = await fetch(url, { signal: controller.signal });
         if (!res.ok) {
           throw new Error(res.statusText);
         }
@@ -19,12 +21,20 @@ export const useFetch = (url) => {
         setData(json);
         setError(null);
       } catch (err) {
-        setIsPending(false);
-        setError('Could not fetch the data');
-        console.log(err.message);
+        if (err.name === 'AbortError') {
+          console.log('The fetch was aborted');
+        } else {
+          setIsPending(false);
+          setError('Could not fetch the data');
+          console.log(err.message);
+        }
       }
     };
     fetchData();
+
+    return () => {
+      controller.abort();
+    };
   }, [url]);
 
   return { data, isPending, error };
