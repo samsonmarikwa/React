d0d5c684230a286a80a363ceffa3f6c211f92181
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-14 21:35:43 -0400

    useHistory to redirect user to home page if an error is encountered

--- multipage-site/src/pages/Article.js
+++ multipage-site/src/pages/Article.js
@@ -1,4 +1,5 @@
-import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import { useFetch } from '../hooks/useFetch';
 
 export default function Article() {
@@ -9,6 +10,17 @@ export default function Article() {
   const url = `http://localhost:3000/articles/${id}`;
 
   const { data: article, isPending, error } = useFetch(url);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (error) {
+      //redirect
+      // history.goBack() - a number of methods are available on the history object
+      setTimeout(() => {
+        history.push('/');
+      }, 2000);
+    }
+  }, [error, history]);
 
   return (
     <div>
