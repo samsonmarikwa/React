2665cc48649650503c73515e1c357bd609fa8cc6
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-14 21:16:41 -0400

    useParam hook to read parameters from the url

--- multipage-site/src/pages/Article.js
+++ multipage-site/src/pages/Article.js
@@ -1,3 +1,26 @@
+import { useParams } from 'react-router-dom';
+import { useFetch } from '../hooks/useFetch';
+
 export default function Article() {
-  return <div>Article page</div>;
+  /*   const params = useParams();
+  const id = params.id; */
+
+  const { id } = useParams();
+  const url = `http://localhost:3000/articles/${id}`;
+
+  const { data: article, isPending, error } = useFetch(url);
+
+  return (
+    <div>
+      {isPending && <div>Laoding...</div>}
+      {error && <div>{error}</div>}
+      {article && (
+        <div>
+          <h2>{article.title}</h2>
+          <p>By {article.author}</p>
+          <p>{article.body}</p>
+        </div>
+      )}
+    </div>
+  );
 }
