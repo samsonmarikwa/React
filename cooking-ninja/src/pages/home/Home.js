770ec40102ec74e12547ec1792fd8e7b6e3758a7
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-19 01:57:00 -0400

    Fetch data using useFetch and display in the Home page

--- cooking-ninja/src/pages/home/Home.js
+++ cooking-ninja/src/pages/home/Home.js
@@ -1,5 +1,19 @@
+import { useFetch } from '../../hooks/useFetch';
 import './Home.css';
 
 export default function Home() {
-  return <div>Home</div>;
+  const {
+    data: recipes,
+    isPending,
+    error,
+  } = useFetch('http://localhost:3000/recipes');
+
+  return (
+    <div className='home'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading Recipes...</p>}
+      {recipes &&
+        recipes.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
+    </div>
+  );
 }
