2c29c5abbbbc3b282c4d903152f56eff216d9a2a
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-19 01:18:28 -0400

    Create pages, css, Navbar component, install react-router-dom@5.3.3 to fix component refresh

---
+++ cooking-ninja/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+      <nav>
+        <NavLink exact to='/' className='brand'>
+          <h1>Cooking Ninja</h1>
+        </NavLink>
+        <NavLink to='/create'>Create Recipe</NavLink>
+      </nav>
+    </div>
+  );
+}
