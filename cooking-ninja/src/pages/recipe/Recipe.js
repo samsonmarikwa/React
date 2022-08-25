0ea73afe47cc3f54fc3870ca3d85a37dab1ca365
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-25 14:11:28 -0400

    Update data in a collection

--- cooking-ninja/src/pages/recipe/Recipe.js
+++ cooking-ninja/src/pages/recipe/Recipe.js
@@ -44,6 +44,12 @@ export default function Recipe() {
     }
   }, [error, history]);
 
+  const handleClick = () => {
+    projectFirestore.collection('recipes').doc(id).update({
+      title: 'Something completely different',
+    });
+  };
+
   return (
     <div className={`recipe ${mode}`}>
       {error && <p className='error'>{error}</p>}
@@ -58,6 +64,7 @@ export default function Recipe() {
             ))}
           </ul>
           <p className='method'>{recipe.method}</p>
+          <button onClick={handleClick}>Update me</button>
         </>
       )}
     </div>
