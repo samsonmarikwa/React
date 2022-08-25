7e601071fde4e3491de121702cce3192c40ba2c0
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-25 16:22:32 -0400

    Setup application, create pages and components

---
+++ my-money/src/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+
+import './App.css';
+import Navbar from './component/Navbar';
+
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/signup'>
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
