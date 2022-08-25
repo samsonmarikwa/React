fa427979bd4db35e3ef2678efba4d391babdb62c
Samson Marikwa <samsonmarikwa@outlook.com>
2022-08-25 14:00:07 -0400

    use snapshop function when loading data into state which will cause a listener to be created from firebase which then uses realtime data in firebase. Any changes to the data collection causes an event to be fire which causes the collection to be reloaded

--- cooking-ninja/src/pages/home/Home.js
+++ cooking-ninja/src/pages/home/Home.js
@@ -23,10 +23,8 @@ export default function Home() {
 
   useEffect(() => {
     setIsPending(true);
-    projectFirestore
-      .collection('recipes')
-      .get()
-      .then((snapshot) => {
+    const unsubsribe = projectFirestore.collection('recipes').onSnapshot(
+      (snapshot) => {
         if (snapshot.empty) {
           setError('No recipes to load');
           setIsPending(false);
@@ -38,11 +36,14 @@ export default function Home() {
           setData(results);
           setIsPending(false);
         }
-      })
-      .catch((err) => {
+      },
+      (err) => {
         setError(err.message);
         setIsPending(false);
-      });
+      }
+    );
+
+    return () => unsubsribe();
   }, []);
 
   return (
